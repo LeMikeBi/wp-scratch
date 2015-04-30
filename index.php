06ed4e7133e1c45391f6a455cbc1440278582077
Le Minh Hien <xlemh00@gmail.com>
2015-04-30 21:14:06 +0200

    wordpress from scratch first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title><?php bloginfo('name') ;?></title>	 <!--this is how we use a template name to create our own wp name-->
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>" type="text/css" media="screen" /> <!-- misto dlouheho linku smerujici k style.css staci tohle PHP -->
+</head>
+
+<body>
+
+
+
+
+</body>
+</html>
